10f5e1b1a532cd8a6697152057a946ed25746856
yuuyuu661 <liuliliuli71@gmail.com>
2025-07-30 13:19:35 +0900

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,6 +1,6 @@
-import os
 from flask import Flask
 from threading import Thread
+import os
 
 app = Flask(__name__)
 
@@ -9,7 +9,7 @@ def home():
     return "I'm alive!", 200
 
 def run():
-    port = int(os.environ.get("PORT", 8080))  # ここが重要！
+    port = int(os.environ.get("PORT", 8080))  # ← 必ずPORT環境変数を使う
     app.run(host="0.0.0.0", port=port)
 
 def keep_alive():
