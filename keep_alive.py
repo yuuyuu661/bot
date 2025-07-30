9c96883bbabd6729d4729d17227fa016ee6eb066
yuuyuu661 <liuliliuli71@gmail.com>
2025-07-30 13:12:26 +0900

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,14 +1,16 @@
+import os
 from flask import Flask
 from threading import Thread
 
-app = Flask('')
+app = Flask(__name__)
 
-@app.route('/')
+@app.route("/", methods=["GET", "HEAD"])
 def home():
-    return "I'm alive", 200
+    return "I'm alive!", 200
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get("PORT", 8080))  # ここが重要！
+    app.run(host="0.0.0.0", port=port)
 
 def keep_alive():
     t = Thread(target=run)
