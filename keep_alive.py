ff399ebe110cd46496ea76089087e30a006d3f01
yuuyuu661 <liuliliuli71@gmail.com>
2025-07-30 13:21:04 +0900

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -10,8 +10,8 @@ def home():
 
 def run():
     port = int(os.environ.get("PORT", 8080))  # ← 必ずPORT環境変数を使う
-    app.run(host="0.0.0.0", port=port)
-
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    
 def keep_alive():
     t = Thread(target=run)
     t.start()
